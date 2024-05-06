a5e41e20c1a638c41f859200ebc4db13fdd06dd1
ABDULLAH <abdullah555523@hotmail.com>
2024-05-06 12:35:29 +0400

    added a controller for contact details

---
+++ src/main/java/com/TRA/tra24Springboot/Controllers/ContactDetailsController.java
@@ -0,0 +1,37 @@
+package com.TRA.tra24Springboot.Controllers;
+
+import com.TRA.tra24Springboot.Models.ContactDetails;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/contact-details")
+public class ContactDetailsController {
+
+    ContactDetails contactDetails = new ContactDetails();
+
+    @GetMapping
+    public ContactDetails addContactDetails(){
+        ContactDetails contactDetails1 = new ContactDetails();
+
+        contactDetails.setEmail("abdullah@email.com");
+        contactDetails.setPhoneNumber("12345678");
+        contactDetails.setFaxNumber("87654321");
+        contactDetails.setAddress("Muscat, 124 rd");
+        contactDetails.setPostalCode("000000000");
+
+        contactDetails = contactDetails1;
+        return contactDetails;
+    }
+
+    @GetMapping("update")
+    public ContactDetails updateContactDetails(){
+
+        ContactDetails contactDetails1 = new ContactDetails();
+        return contactDetails1;
+
+    }
+
+
+
+}
