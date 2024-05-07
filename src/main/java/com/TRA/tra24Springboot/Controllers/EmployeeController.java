07e64926763619285e7aa013df050f7f4c29e22b
ABDULLAH <abdullah555523@hotmail.com>
2024-05-07 11:35:37 +0400

    added the add function for the Employee Controller

--- src/main/java/com/TRA/tra24Springboot/Controllers/EmployeeController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/EmployeeController.java
@@ -1,4 +1,36 @@
 package com.TRA.tra24Springboot.Controllers;
 
+import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.Employee;
+import com.TRA.tra24Springboot.Models.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+@RestController
+@RequestMapping("Employee")
 public class EmployeeController {
+    Employee employee = new Employee();
+
+    @GetMapping("add")
+    public Employee addEmployee(){
+        Employee employeeAdded = new Employee();
+        User user = new User();
+        user.setName("Abdullah");
+        user.setId(1);
+        user.setAccessPrivileges("drx");
+
+        employeeAdded.setId(1);
+        employeeAdded.setShift("9:00am");
+        employeeAdded.setDepartment("Networks and Security");
+        employeeAdded.setLocation("Muscat");
+        employeeAdded.setJobTitle("Cyber Security Assistant");
+        employeeAdded.setCivilID("1234567");
+        employeeAdded.setUser(user);
+
+        employee = employeeAdded;
+        return employee;
+    }
+
+
+
 }
