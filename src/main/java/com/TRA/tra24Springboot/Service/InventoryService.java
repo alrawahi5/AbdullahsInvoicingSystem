737abdb155444dc6f83e86364693011bd34fea9f
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-13 14:31:00 +0400

    added a service file for Inventory

--- src/main/java/com/TRA/tra24Springboot/Service/InventoryService.java
+++ src/main/java/com/TRA/tra24Springboot/Service/InventoryService.java
@@ -1,2 +1,48 @@
-package com.TRA.tra24Springboot.Service;public class InventoryService {
+package com.TRA.tra24Springboot.Service;
+
+import com.TRA.tra24Springboot.Models.Inventory;
+import com.TRA.tra24Springboot.Repository.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class InventoryService {
+
+    @Autowired
+    InventoryRepository inventoryRepository;
+
+    public Inventory addInventory(Inventory inventory){
+
+        List product = new ArrayList<>();
+        List workers = new ArrayList<>();
+
+        inventory.setManager("Abdullah");
+        inventory.setLocation("Muscat");
+        inventory.setPhoneNumber("11111");
+        inventory.setId(1);
+        inventory.setProducts(product);
+        inventory.setOpeningHours("9:00am-2:00pm");
+        inventory.setSupplier("Omantel");
+        inventory.setClosingHours("Weekend hours");
+        inventory.setWorkers(workers);
+        inventory.setCreatedDate(new Date());
+
+        return inventoryRepository.save(inventory);
+    }
+
+    /*public String deleteInventory(Integer id) {
+        Inventory inventory = inventoryRepository.findById(id).get();
+        if (inventory.getId().equals(id)) {
+            inventory.setIsActive(Boolean.FALSE);
+            System.out.println(inventory.toString());
+        }
+        return "Success!";
+    }*/
 }
