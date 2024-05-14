6116ce5151bd29ecd817369db4a4d1059ea3ce7a
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-14 13:14:38 +0400

    added an update function for Order

--- src/main/java/com/TRA/tra24Springboot/Controllers/OrderController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/OrderController.java
@@ -3,9 +3,7 @@ package com.TRA.tra24Springboot.Controllers;
 import com.TRA.tra24Springboot.Models.Order;
 import com.TRA.tra24Springboot.Service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("Order")
@@ -15,7 +13,18 @@ public class OrderController {
     OrderService orderService;
 
     @PostMapping("add")
-    public Order addOrder(Order order){
+    public Order addOrder(Order order) {
         return orderService.addOrder(order);
+    }
+
+    @PutMapping("update")
+    public Order updateOrder(@RequestBody Order order){
+        return orderService.updateOrder(order);
+    }
+
+    @PostMapping("delete")
+    public String deleteOrder(@RequestParam Integer id) {
+        orderService.deleteOrder(id);
+        return "Success";
     }
 }
