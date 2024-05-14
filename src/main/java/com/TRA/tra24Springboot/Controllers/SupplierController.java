9fa5b4a24b13e5e571d0c174fa5631ac2ee0689d
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-14 13:46:58 +0400

    added a controller for supplier with all its functions

--- src/main/java/com/TRA/tra24Springboot/Controllers/SupplierController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/SupplierController.java
@@ -1,4 +1,33 @@
 package com.TRA.tra24Springboot.Controllers;
 
+import com.TRA.tra24Springboot.Models.Supplier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("supplier")
 public class SupplierController {
+
+    @Autowired
+    SupplierService supplierService;
+
+    @PostMapping("add")
+    public SupplierController addSupplier(Supplier supplier){
+        return supplierService.addSupplier(supplier);
+    }
+
+    @PostMapping("delete")
+    public SupplierController deleteSupplier(@RequestParam Supplier supplier){
+        return supplierService.deleteSupplier(supplier);
+    }
+
+    @PutMapping("update")
+    public SupplierController updateSupplier(@RequestBody Supplier supplier){
+        return supplierService.updateSupplier(supplier);
+    }
+
+    @GetMapping("get")
+    public List<SupplierDOT> getSchool(){
+        return supplierService.getSchools();
+}
 }
