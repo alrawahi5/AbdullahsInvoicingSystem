947e54e5fd93caac889f349222d8b21fdedef5f3
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-15 13:38:33 +0400

    added DTO for user

--- src/main/java/com/TRA/tra24Springboot/DTO/UserDTO.java
+++ src/main/java/com/TRA/tra24Springboot/DTO/UserDTO.java
@@ -1,2 +1,18 @@
-package com.TRA.tra24Springboot.DTO;public class UserDTO {
+package com.TRA.tra24Springboot.DTO;
+
+import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.UserTypes;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+public class UserDTO {
+
+    String name;
+    @Enumerated(EnumType.STRING)
+    UserTypes userType;
+    ContactDetails contactDetails;
+    String accessPrivileges;
+
+    
+
 }
