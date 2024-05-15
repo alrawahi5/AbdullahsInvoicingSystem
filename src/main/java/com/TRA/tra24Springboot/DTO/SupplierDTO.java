4bf96c2d78f2ea40b9a4e37bd062469281db400a
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-15 12:15:09 +0400

    added the DOT file with both functions for the supplier

--- src/main/java/com/TRA/tra24Springboot/DTO/SupplierDTO.java
+++ src/main/java/com/TRA/tra24Springboot/DTO/SupplierDTO.java
@@ -1,2 +1,52 @@
-package com.TRA.tra24Springboot.DTO;public class SupplierDTO {
+package com.TRA.tra24Springboot.DTO;
+
+import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.Order;
+import com.TRA.tra24Springboot.Models.Product;
+import com.TRA.tra24Springboot.Models.Supplier;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+@Data
+public class SupplierDTO {
+    String companyName;
+    String country;
+    ContactDetails contactDetails;
+    List<Product> productsOffered;
+    Date nextDeliveryTime;
+    List<Product> expectedProducts;
+    String complaints;
+    String paymentMethods;
+    String shippingMethods;
+    String minimumOrderQuantity;
+    List<Order> orders;
+
+    public static SupplierDTO convertToDTO(Supplier supplier) {
+        SupplierDTO supplierDTO = new SupplierDTO();
+        supplierDTO.setCompanyName(supplier.getCompanyName());
+        supplierDTO.setCountry(supplierDTO.getCountry());
+        supplierDTO.setContactDetails(supplier.getContactDetails());
+        supplierDTO.setProductsOffered(supplier.getProductsOffered());
+        supplierDTO.setNextDeliveryTime(supplier.getNextDeliveryTime());
+        supplierDTO.setExpectedProducts(supplier.getExpectedProducts());
+        supplierDTO.setComplaints(supplierDTO.getComplaints());
+        supplierDTO.setPaymentMethods(supplierDTO.getPaymentMethods());
+        supplierDTO.setShippingMethods(supplierDTO.getShippingMethods());
+        supplierDTO.setMinimumOrderQuantity(supplierDTO.getMinimumOrderQuantity());
+        supplierDTO.setOrders(supplier.getOrders());
+        return supplierDTO;
+    }
+
+    public static List<SupplierDTO> convertToDTO(List<Supplier> supplierList) {
+        List<SupplierDTO> supplierDOTS = new ArrayList<>();
+        for (Supplier oldFromTheDB : supplierList) {
+            SupplierDTO dto = convertToDTO(oldFromTheDB);
+            supplierDOTS.add(dto);
+        }
+        return supplierDOTS;
+    }
 }
