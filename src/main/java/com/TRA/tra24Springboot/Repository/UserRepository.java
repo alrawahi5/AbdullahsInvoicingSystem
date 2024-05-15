7c9a1608eec78143f9ddc4a61fdf1b620f05530a
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-15 13:40:29 +0400

    added a repository for user

--- src/main/java/com/TRA/tra24Springboot/Repository/UserRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.TRA.tra24Springboot.Repository;public interface UserRepository {
+package com.TRA.tra24Springboot.Repository;
+
+import com.TRA.tra24Springboot.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
