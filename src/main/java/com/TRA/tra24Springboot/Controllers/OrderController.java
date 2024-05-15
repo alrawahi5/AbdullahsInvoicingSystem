8a835b744b660cac2aad9107a6a288077f8122c5
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-15 12:23:28 +0400

    added the get function for order

--- src/main/java/com/TRA/tra24Springboot/Controllers/OrderController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/OrderController.java
@@ -1,10 +1,13 @@
 package com.TRA.tra24Springboot.Controllers;
 
+import com.TRA.tra24Springboot.DTO.SupplierDTO;
 import com.TRA.tra24Springboot.Models.Order;
 import com.TRA.tra24Springboot.Service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.TRA.tra24Springboot.DTO.OrderDOT;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("Order")
@@ -27,5 +30,10 @@ public class OrderController {
     public String deleteOrder(@RequestParam Integer id) {
         orderService.deleteOrder(id);
         return "Success";
+    }
+
+    @GetMapping("get")
+    public List<OrderDOT> getOrder(){
+        return orderService.getOrder();
     }
 }
