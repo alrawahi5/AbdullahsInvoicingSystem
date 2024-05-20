8a0e55da32b0e9510c374cb93c94c8834fa1eed4
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-20 13:47:47 +0400

    added the add function in the ProductDetails controller file

--- src/main/java/com/TRA/tra24Springboot/Controllers/ProductDetailsController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/ProductDetailsController.java
@@ -1,16 +1,42 @@
 package com.TRA.tra24Springboot.Controllers;
 
+import com.TRA.tra24Springboot.DTO.UserDTO;
+import com.TRA.tra24Springboot.Models.ProductDetails;
+import com.TRA.tra24Springboot.Models.User;
 import com.TRA.tra24Springboot.Service.ProductDetailsService;
 import com.TRA.tra24Springboot.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("productDetails")
 public class ProductDetailsController {
 
     @Autowired
     ProductDetailsService productDetailsuserService;
 
-}
+    @PostMapping("add")
+    public ProductDetails addProdDetails(ProductDetails productDetails) {
+        return productDetailsuserService.addProdDetails(productDetails);
+    }
+
+    /*@PostMapping("delete")
+    public String deleteUser(@RequestParam Integer id) {
+        userService.deleteUser(id);
+        return "Success";
+    }
+
+    @PutMapping("update")
+    public User updateUser(@RequestBody User user) {
+        return userService.updateUser(user);
+    }
+
+    @GetMapping("get")
+    public List<UserDTO> getUser() {
+        return userService.getUser();*/
+    }
+
+
+
