56809708476a42c8d6558b591fcfd88d12aa3a8b
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-20 09:57:17 +0400

    added a delete function for the user service

--- src/main/java/com/TRA/tra24Springboot/Service/UserService.java
+++ src/main/java/com/TRA/tra24Springboot/Service/UserService.java
@@ -1,6 +1,7 @@
 package com.TRA.tra24Springboot.Service;
 
 import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.Order;
 import com.TRA.tra24Springboot.Models.User;
 import com.TRA.tra24Springboot.Models.UserTypes;
 import com.TRA.tra24Springboot.Repository.UserRepository;
@@ -24,5 +25,15 @@ public class UserService {
         user.setContactDetails(user.getContactDetails());
         user.setAccessPrivileges("drx");
         return userRepository.save(user);
+    }
+
+    public String deleteUser(Integer id) {
+        User user = userRepository.findById(id).get();
+
+        if (user.getId().equals(id)) {
+            user.setIsActive(Boolean.FALSE);
+            System.out.println(user.toString());
+        }
+        return "Success";
     }
 }
