11a30dee5317ca1d258f54e9ed07fd1497812d25
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-20 13:18:39 +0400

    added a repository obj in ProdDetails service file

--- src/main/java/com/TRA/tra24Springboot/Service/ProductDetailsService.java
+++ src/main/java/com/TRA/tra24Springboot/Service/ProductDetailsService.java
@@ -1,6 +1,5 @@
 package com.TRA.tra24Springboot.Service;
 
-import com.TRA.tra24Springboot.Models.ProductDetails;
 import com.TRA.tra24Springboot.Repository.ProductDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
