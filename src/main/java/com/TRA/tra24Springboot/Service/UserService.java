c46ed1caec5c2ab4943e3f761c77b2863b0ec3f2
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-20 09:50:13 +0400

    added a service with add function for user

--- src/main/java/com/TRA/tra24Springboot/Service/UserService.java
+++ src/main/java/com/TRA/tra24Springboot/Service/UserService.java
@@ -1,4 +1,28 @@
 package com.TRA.tra24Springboot.Service;
 
+import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.User;
+import com.TRA.tra24Springboot.Models.UserTypes;
+import com.TRA.tra24Springboot.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User adduser(User user) {
+
+        String name;
+        UserTypes userType;
+        ContactDetails contactDetails;
+        String accessPrivileges;
+        user.setName("Abdullah");
+        user.setUserType(user.getUserType());
+        user.setContactDetails(user.getContactDetails());
+        user.setAccessPrivileges("drx");
+        return userRepository.save(user);
+    }
 }
