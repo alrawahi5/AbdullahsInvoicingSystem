6272ccf29a9eaabce9da44821df2f4e085cfc78b
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-20 13:48:23 +0400

    added the add function in the ProductDetails service file

--- src/main/java/com/TRA/tra24Springboot/Service/ProductDetailsService.java
+++ src/main/java/com/TRA/tra24Springboot/Service/ProductDetailsService.java
@@ -1,12 +1,31 @@
 package com.TRA.tra24Springboot.Service;
 
+import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.ProductDetails;
+import com.TRA.tra24Springboot.Models.User;
+import com.TRA.tra24Springboot.Models.UserTypes;
 import com.TRA.tra24Springboot.Repository.ProductDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class ProductDetailsService {
 
     @Autowired
     ProductDetailsRepository productDetailsRepository;
+
+    public ProductDetails addProdDetails(ProductDetails productDetails) {
+
+
+        productDetails.setName("Abdullah");
+        productDetails.setCountryOfOrigin("Oman");
+        productDetails.setExpiryDate(new Date());
+        productDetails.setSize("12");
+        productDetails.setColor("purple");
+        productDetails.setDescription("electronics");
+        productDetails.setPrice(198.8);
+        return productDetailsRepository.save(productDetails);
+    }
 }
