09e8771a4d114637ae29c2f2d5c2c7473fe97355
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-20 12:26:48 +0400

    added a file controller for Product details

--- src/main/java/com/TRA/tra24Springboot/Controllers/ProductDetailsController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/ProductDetailsController.java
@@ -1,2 +1,10 @@
-package com.TRA.tra24Springboot.Controllers;public class ProductDetailsController {
+package com.TRA.tra24Springboot.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class ProductDetailsController {
+    
 }
