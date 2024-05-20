e9b02ac0c60d04c3ac94c71fac96b6495cebbbc6
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-20 09:35:23 +0400

    added the DTO for user

--- src/main/java/com/TRA/tra24Springboot/DTO/UserDTO.java
+++ src/main/java/com/TRA/tra24Springboot/DTO/UserDTO.java
@@ -1,18 +1,38 @@
 package com.TRA.tra24Springboot.DTO;
 
 import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.User;
 import com.TRA.tra24Springboot.Models.UserTypes;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
 public class UserDTO {
 
     String name;
-    @Enumerated(EnumType.STRING)
     UserTypes userType;
     ContactDetails contactDetails;
     String accessPrivileges;
 
-    
+    public static UserDTO convertToDTO(User user) {
+        UserDTO userDOT = new UserDTO();
+        userDOT.setName(user.getName());
+        userDOT.setUserType(user.getUserType());
+        userDOT.setContactDetails(user.getContactDetails());
+        user.setAccessPrivileges(user.getAccessPrivileges());
+
+        return userDOT;
+    }
+
+    public static List<UserDTO> convertToDTO(List<User> userList) {
+        List<UserDTO> userDOTS = new ArrayList<>();
+        for (User oblFromTheDB : userList) {
+            UserDTO dto = convertToDTO(oblFromTheDB);
+            userDOTS.add(dto);
+        }
+        return userDOTS;
+    }
 
 }
