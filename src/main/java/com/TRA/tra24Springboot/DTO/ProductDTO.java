d2ff7b675e01b069d66eed5626f6d4a8193fda7a
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-26 12:33:12 +0400

    added a DTO  for Product

--- src/main/java/com/TRA/tra24Springboot/DTO/ProductDTO.java
+++ src/main/java/com/TRA/tra24Springboot/DTO/ProductDTO.java
@@ -1,2 +1,36 @@
-package com.TRA.tra24Springboot.DTO;public class ProductDTO {
+package com.TRA.tra24Springboot.DTO;
+
+import com.TRA.tra24Springboot.Models.Product;
+import com.TRA.tra24Springboot.Models.ProductDetails;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class ProductDTO {
+
+    Integer quantity;
+    String category;
+    UUID sku;
+    ProductDetails productDetails;
+
+    public static ProductDTO convertToDTO(Product product) {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setQuantity(product.getQuantity());
+        productDTO.setCategory(product.getCategory());
+        productDTO.setSku(product.getSku());
+        productDTO.setProductDetails(product.getProductDetails());
+        return productDTO;
+    }
+
+    public static List<ProductDTO> convertToDTO(List<Product> productList) {
+        List<ProductDTO> productrDOTS = new ArrayList<>();
+        for (Product oldFromTheDB : productList) {
+            ProductDTO dto = convertToDTO(oldFromTheDB);
+            productrDOTS.add(dto);
+        }
+        return productrDOTS;
+    }
 }
