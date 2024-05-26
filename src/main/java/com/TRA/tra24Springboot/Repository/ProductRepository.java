3641dfdbc0921c156660656b9de92cbe17d482c7
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-26 12:32:24 +0400

    added the query to get the list via the category for product

--- src/main/java/com/TRA/tra24Springboot/Repository/ProductRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/ProductRepository.java
@@ -1,7 +1,15 @@
 package com.TRA.tra24Springboot.Repository;
 
 import com.TRA.tra24Springboot.Models.Product;
+import com.TRA.tra24Springboot.Models.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query("SELECT p from Product p WHERE p.category =:category")
+    List<Product> getByCategory(@Param("category") String category);
 }
