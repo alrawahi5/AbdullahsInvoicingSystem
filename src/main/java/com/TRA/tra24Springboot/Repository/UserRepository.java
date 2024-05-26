112f864d21ab1ddf0daa926b08e05bc1ce49533b
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-26 10:37:21 +0400

    added the query in the user repositoy

--- src/main/java/com/TRA/tra24Springboot/Repository/UserRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.TRA.tra24Springboot.Repository;
 
 import com.TRA.tra24Springboot.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u from User u WHERE u.name =:name")
+    List<User> getByUserName(@Param("name") String userName);
 }
