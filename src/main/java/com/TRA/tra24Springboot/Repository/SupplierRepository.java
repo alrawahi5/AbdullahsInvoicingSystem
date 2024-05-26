7598230a20554d4316d96b78692e699d7dd92af5
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-26 12:09:20 +0400

    added the query in the Supplier repositoy

--- src/main/java/com/TRA/tra24Springboot/Repository/SupplierRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/SupplierRepository.java
@@ -2,7 +2,14 @@ package com.TRA.tra24Springboot.Repository;
 
 import com.TRA.tra24Springboot.Models.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+
+    @Query("SELECT cn from Supplier cn WHERE cn.companyName =:cName")
+    List<Supplier> getByCompanyName(@Param("cName") String cn);
 }
