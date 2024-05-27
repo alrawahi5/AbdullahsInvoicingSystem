ce789bd6aed93f9e1799e1d9c308d9a9cee73922
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-27 09:46:30 +0400

    added a getbyName query for ProdDetails

--- src/main/java/com/TRA/tra24Springboot/Repository/ProductDetailsRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/ProductDetailsRepository.java
@@ -3,6 +3,12 @@ package com.TRA.tra24Springboot.Repository;
 import com.TRA.tra24Springboot.Models.ProductDetails;
 import com.TRA.tra24Springboot.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductDetailsRepository extends JpaRepository<ProductDetails, Integer> {
+    @Query("SELECT pd from ProductDetails pd WHERE pd.name =:name")
+    List<ProductDetails> getByProductName(@Param("name") String name);
 }
