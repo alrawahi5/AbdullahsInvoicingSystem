b0daeffd2fe4d59ead93ce66dcf69fc884690dba
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-27 13:38:26 +0400

    added a getbyName query for Inventory

--- src/main/java/com/TRA/tra24Springboot/Repository/InventoryRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/InventoryRepository.java
@@ -2,7 +2,15 @@ package com.TRA.tra24Springboot.Repository;
 
 import com.TRA.tra24Springboot.Models.ContactDetails;
 import com.TRA.tra24Springboot.Models.Inventory;
+import com.TRA.tra24Springboot.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
+
+    @Query("SELECT p from Inventory p WHERE p.phoneNumber =:phoneNumber")
+    List<Inventory> getByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 }
