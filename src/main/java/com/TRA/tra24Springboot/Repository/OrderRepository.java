9d09846b5d9c3893f4c1728f7f6c5ff3d256ab3b
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-27 10:41:33 +0400

    added a getbyDescription query for Order

--- src/main/java/com/TRA/tra24Springboot/Repository/OrderRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/OrderRepository.java
@@ -1,7 +1,15 @@
 package com.TRA.tra24Springboot.Repository;
 
 import com.TRA.tra24Springboot.Models.Order;
+import com.TRA.tra24Springboot.Models.ProductDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    @Query("SELECT o from Order o WHERE o.description =:description")
+    List<Order> getByDescriptione(@Param("description") String description);
 }
