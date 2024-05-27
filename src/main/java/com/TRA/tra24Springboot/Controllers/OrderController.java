248a019c67433cfef162bcaa0c80ce4369a36ac2
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-27 10:40:11 +0400

    added the query helper for order in the order controller

--- src/main/java/com/TRA/tra24Springboot/Controllers/OrderController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/OrderController.java
@@ -1,6 +1,7 @@
 package com.TRA.tra24Springboot.Controllers;
 
 import com.TRA.tra24Springboot.DTO.OrderDOT;
+import com.TRA.tra24Springboot.DTO.ProdDetailsDTO;
 import com.TRA.tra24Springboot.Models.Order;
 import com.TRA.tra24Springboot.Service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +35,10 @@ public class OrderController {
     @GetMapping("get")
     public List<OrderDOT> getOrder() {
         return orderService.getOrder();
+    }
+
+    @GetMapping("getByDescription")
+    public List<OrderDOT> getByDescriptione(@RequestParam String description) {
+        return orderService.getByDescriptione(description);
     }
 }
