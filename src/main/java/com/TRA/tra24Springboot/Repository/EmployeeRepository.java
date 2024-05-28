0b87816517e07b4e4904d51ecd82c1980de642e1
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-05-28 13:48:20 +0400

    added a query to get an employee by civilID

--- src/main/java/com/TRA/tra24Springboot/Repository/EmployeeRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/EmployeeRepository.java
@@ -1,7 +1,15 @@
 package com.TRA.tra24Springboot.Repository;
 
 import com.TRA.tra24Springboot.Models.Employee;
+import com.TRA.tra24Springboot.Models.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    @Query("SELECT e from Employee e WHERE e.civilID =:civilID")
+    List<Employee> getByCivilID(@Param("civilID") String civilID);
 }
