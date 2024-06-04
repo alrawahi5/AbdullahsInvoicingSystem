6bbc548f0ebcb3985ddcd262884c45a61144b24b
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-06-04 10:26:55 +0400

    added a crom to return every 5 minuts

--- src/main/java/com/TRA/tra24Springboot/Service/SlackService.java
+++ src/main/java/com/TRA/tra24Springboot/Service/SlackService.java
@@ -4,7 +4,10 @@ import com.slack.api.Slack;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.security.PublicKey;
 
 @Service
 public class SlackService {
@@ -13,6 +16,14 @@ public class SlackService {
 
         @Value("${slack.token}")
         private String slackToken;
+
+        @Scheduled(cron = "0 5 * * * *")
+        public void cronSchudualing(){
+            String channel= ""; //#abdullah_channel
+            String message = "every 5 minute cron test";
+
+            sendMessage(channel, message);
+        }
 
         public void sendMessage(String channel, String message) {
             Slack slack = Slack.getInstance();
