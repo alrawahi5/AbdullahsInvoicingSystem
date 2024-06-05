626712667c0010cc7f434e4277eb8717c1f22c91
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-06-05 10:34:39 +0400

    added a repository for contact details

--- src/main/java/com/TRA/tra24Springboot/Repository/ContactDetailsRepository.java
+++ src/main/java/com/TRA/tra24Springboot/Repository/ContactDetailsRepository.java
@@ -1,7 +1,15 @@
 package com.TRA.tra24Springboot.Repository;
 
 import com.TRA.tra24Springboot.Models.ContactDetails;
+import com.TRA.tra24Springboot.Models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ContactDetailsRepository extends JpaRepository<ContactDetails, Integer> {
+
+    @Query("SELECT p from ContactDetails p WHERE p.phoneNumber =:phoneNumber")
+    List<ContactDetails> getByPhoneNumber(@Param("phoneNumber") String phoneNumber);
 }
