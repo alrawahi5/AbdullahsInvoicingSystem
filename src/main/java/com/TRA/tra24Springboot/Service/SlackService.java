1b0c4a0e9401215efa4d29735a5627a040017fe7
Abdullah Alrawahi <abdullah555523@hotmail.com>
2024-06-05 12:30:18 +0400

    added a cron for every 1st day of the mo at 3am

--- src/main/java/com/TRA/tra24Springboot/Service/SlackService.java
+++ src/main/java/com/TRA/tra24Springboot/Service/SlackService.java
@@ -17,8 +17,9 @@ public class SlackService {
         @Value("${slack.token}")
         private String slackToken;
 
-    //@Scheduled(cron = "0 30 14 * * *") // every day at 2:30pm
-        @Scheduled(cron = "0 0 8 * * 1") // every Monday at 8:00am
+        //@Scheduled(cron = "0 30 14 * * *") // every day at 2:30pm
+        //@Scheduled(cron = "0 0 8 * * 1") // every Monday at 8:00am
+        @Scheduled(cron = "0 0 3 1 * 0") // every first day of the month at 3 am
         public void cronSchudualing(){
             String channel= ""; //#abdullah_channel
             String message = "every 5 minute cron test";
